e4dc51e455b153890a1ed49fbd83a67bfb062891
amal-hafis <114781073+amal-hafis@users.noreply.github.com>
2023-09-02 14:12:22 +0400

    Add files via upload

---
+++ public/server.js
@@ -0,0 +1,35 @@
+
+
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: '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'
+});
+
+
+
+
+const express =  require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.listen(5000, ()=> {
+    console.log("Server is active")
+})
+
+
+app.post('/chat', async (req, res)=> {   
+  try {
+    const resp = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+        messages: [
+          { role: "user", content: req.body.question}
+        ]  
+    })           
+        
+    res.status(200).json({message: resp.data.choices[0].message.content})
+  } catch(e) {
+      res.status(400).json({message: e.message})
+  }
+})
